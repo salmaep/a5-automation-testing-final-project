871c23ad544744d7179fcd8f3aaa00c6db867aab
Tabitha <tabitha.salsabila.tif421@polban.ac.id>
2024-06-01 20:12:32 +0700

    add web testing test case (feature: login)

--- src/test/java/io/cucumber/StepDefinitions/web/StepDefinitions.java
+++ src/test/java/io/cucumber/StepDefinitions/web/StepDefinitions.java
@@ -19,6 +19,14 @@ public class StepDefinitions {
     private LoginPage loginPage;
     private DashboardPage dashboardPage;
 
+    @Given("I am on the login page")
+    public void i_am_on_the_login_page() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+        loginPage = new LoginPage(driver);
+    }
+
     @Given("the user is logged in to the application with username {string} and password {string}")
     public void the_user_is_logged_in_to_the_application(String username, String password) {
         i_am_on_the_login_page();
@@ -28,17 +36,58 @@ public class StepDefinitions {
         dashboardPage = new DashboardPage(driver);
     }
 
-    @Given("I am on the login page")
-    public void i_am_on_the_login_page() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.get("https://www.saucedemo.com/");
-        loginPage = new LoginPage(driver);
-    }
-
     @Given("the user is on the dashboard page")
     public void the_user_is_on_the_dashboard_page() {
         // Covered in previous step
+    }
+
+    @When("I do not enter username and password")
+    public void i_do_not_enter_username_and_password() {
+        // No action needed as we are not entering anything
+    }
+
+    @Then("I should see a username is required error message")
+    public void i_should_see_a_username_is_required_error_message() {
+        String expectedErrorMessage = "You need Username & Password!";
+        String actualErrorMessage = loginPage.getErrorMessage();
+        assertEquals(expectedErrorMessage, actualErrorMessage);
+    }
+
+    @When("I enter registered username with incorrect password")
+    public void i_enter_registered_username_with_incorrect_password() {
+        loginPage.enterUsername("standard_user");
+        loginPage.enterPassword("incorrectPassword");
+        loginPage.clickLoginButton();
+    }
+
+    @Then("I should see an error message for incorrect password")
+    public void i_should_see_an_error_message_for_incorrect_password() {
+        String expectedErrorMessage = "Username and password do not match any user in this service!";
+        String actualErrorMessage = loginPage.getErrorMessage();
+        assertEquals(expectedErrorMessage, actualErrorMessage);
+    }
+
+    @When("I enter {string} and {string}")
+    public void i_enter_username_and_password(String username, String password) {
+        loginPage.enterUsername(username);
+        loginPage.enterPassword(password);
+    }
+
+    @When("I click the login button")
+    public void i_click_the_login_button() {
+        loginPage.clickLoginButton();
+    }
+
+    @Then("I should be redirected to the dashboard page")
+    public void i_should_be_redirected_to_the_dashboard_page() {
+        String currentUrl = driver.getCurrentUrl();
+        assert currentUrl.contains("inventory");
+    }
+
+    @Then("I should see the error message {string}")
+    public void i_should_see_the_error_message(String expectedErrorMessage) {
+        String actualErrorMessage = loginPage.getErrorMessage();
+        assertEquals(expectedErrorMessage, actualErrorMessage);
     }
 
     @When("the user clicks the menu icon on the top left corner of the dashboard")
--- src/test/java/io/cucumber/locators/web/PageLocators.java
+++ src/test/java/io/cucumber/locators/web/PageLocators.java
@@ -6,22 +6,30 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.WebDriver;
 
 public class PageLocators {
+    // Locators for LoginPage
+    @FindBy(xpath = "//input[@id='user-name']")
+    public WebElement usernameField;
+
+    @FindBy(xpath = "//input[@id='password']")
+    public WebElement passwordField;
+
+    @FindBy(xpath = "//input[@id='login-button']")
+    public WebElement loginButton;
 
     @FindBy(xpath = "//h3[@data-test='error']")
     public WebElement errorMessage;
 
-    // Locators for DashboardPage
-    @FindBy(id = "react-burger-menu-btn")
+    @FindBy(xpath = "//button[@id='react-burger-menu-btn']")
     public WebElement menuIcon;
+
+    @FindBy(xpath = "//a[@id='logout_sidebar_link']")
+    public WebElement logoutMenu;
 
     @FindBy(id = "inventory_sidebar_link")
     public WebElement AllItemsMenu;
 
     @FindBy(id = "about_sidebar_link")
     public WebElement aboutMenu;
-
-    @FindBy(id = "logout_sidebar_link")
-    public WebElement logoutMenu;
 
     // Constructor to initialize elements
     public PageLocators(WebDriver driver) {
